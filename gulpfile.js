5519ed8e230f46418e58558afe17bb71caf2ad05
ALeonoff80 <a.leonov80@gmail.com>
2016-12-06 22:57:40 +0500

    2016-12-06 add gulp sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// sass compile
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+	gulp.src('css/main.scss')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(function(f) {
+			return f.base;
+		}))
+});
+
+gulp.task('default', ['sass'], function() {
+	gulp.watch('css/main.scss', ['sass']);
+})
+
+/*
+	outputStyle types:
+		nested
+		compact
+		expanded
+		compressed
+*/
