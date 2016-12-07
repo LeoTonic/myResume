400f35c317aee9fb9b6864d50ecf82754ac005d5
ALeonoff80 <a.leonov80@gmail.com>
2016-12-07 18:43:24 +0500

    2016-12-07 work without adaptive complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var prefix = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	gulp.src('css/main.scss')
 		.pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+		.pipe(prefix({browsers: ["> 1%", "ie 8", "Firefox ESR"]}))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(function(f) {
 			return f.base;
